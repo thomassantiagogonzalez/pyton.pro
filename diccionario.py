1578f7158fd876da183ba5c246a622e1cc1c92c4
thomassantiagogonzalez <thomas.gonzero@gmail.com>
2026-04-24 19:40:41 -0500

    Update and rename README.md to diccionario.py

---
+++ diccionario.py
@@ -0,0 +1,14 @@
+# thomas santiago
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            "XD": "una respuesta que simula que le dio gracia algo que dijiste"
+            "UD o ud": "se refiere a la palabra usted"
+            }
+
+word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+
+if word in meme_dict.keys():
+    # ¿Qué debemos hacer si se encuentra la palabra?
+else:
+    # ¿Qué hacer si no se encuentra la palabra?
